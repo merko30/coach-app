cbad79b8f3685a31ad5623a435f2b2507de55a80
Merim <merim.hasanbegovic@outlook.com>
2025-09-02 15:35:23 +0200

    feat: style form

--- src/components/PlanForm/index.tsx
+++ src/components/PlanForm/index.tsx
@@ -1,5 +1,4 @@
 import {
-  Formik,
   Form,
   FieldArray,
   Field,
@@ -17,13 +16,13 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import { Card, CardHeader } from "@/components/ui/card";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
-import { Trash } from "lucide-react";
-import DayFieldsForm from "./DayFieldsForm";
+import DayFieldsForm from "./WeekForm";
 import { initialValues, type PlanFormValues } from "./constants";
 import { SortableItem } from "./SortableItem";
 import { uuidv7 } from "uuidv7";
+import { FormikSelect } from "../FormikSelect";
 
 export function PlanForm({
   onSubmit,
@@ -41,93 +40,114 @@ export function PlanForm({
 
   return (
     <FormikProvider value={formik}>
-      <Form className="space-y-6">
-        <div className="space-y-2">
-          <Label htmlFor="title">Title</Label>
-          <Input name="title" />
-          {getIn(touched, "title") && getIn(errors, "title") && (
-            <div className="text-red-500 text-xs">{getIn(errors, "title")}</div>
-          )}
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="description">Description</Label>
-          <Textarea name="description" />
-          {getIn(touched, "description") && getIn(errors, "description") && (
-            <div className="text-red-500 text-xs">
-              {getIn(errors, "description")}
-            </div>
-          )}
-        </div>
-        <div className="space-y-2">
-          <Label htmlFor="level">Level</Label>
-          <Field as="select" name="level" className="input">
-            <option value="BEGINNER">Beginner</option>
-            <option value="INTERMEDIATE">Intermediate</option>
-            <option value="ADVANCED">Advanced</option>
-          </Field>
-        </div>
-        {/* Weeks DnD */}
-        <FieldArray name="weeks">
-          {({ push, remove, move }) => (
-            <DndContext
-              collisionDetection={closestCenter}
-              onDragEnd={(event) => {
-                const { active, over } = event;
-
-                if (active.id !== over?.id) {
-                  const oldIndex = values.weeks.findIndex(
-                    (w) => w.id === active.id
-                  );
-                  const newIndex = values.weeks.findIndex(
-                    (w) => w.id === over?.id
-                  );
-                  move(oldIndex, newIndex);
-                  // update order
-                  console.log(
-                    arrayMove(values.weeks, oldIndex, newIndex).map((w, i) => ({
-                      ...w,
-                      order: i,
-                    }))
-                  );
-                  setFieldValue(
-                    "weeks",
-                    arrayMove(values.weeks, oldIndex, newIndex).map((w, i) => ({
-                      ...w,
-                      order: i,
-                    }))
-                  );
-                }
-              }}
-            >
-              <SortableContext
-                items={values.weeks.map((w) => w.id!)}
-                strategy={verticalListSortingStrategy}
-              >
-                <div className="space-y-4">
-                  {values.weeks.map((week, weekIdx) => (
-                    <SortableItem key={week.order} id={week.id!}>
-                      <DayFieldsForm weekIdx={weekIdx} remove={remove} />
-                    </SortableItem>
-                  ))}
-                  <Button
-                    type="button"
-                    onClick={() =>
-                      push({
-                        order: values.weeks.length,
-                        days: [],
-                        id: uuidv7(),
-                      })
-                    }
-                  >
-                    Add Week
-                  </Button>
+      <Card>
+        <CardHeader>
+          <h1 className="text-2xl">Create plan</h1>
+        </CardHeader>
+        <CardContent>
+          <Form className="space-y-6">
+            <div className="space-y-2">
+              <Label htmlFor="title">Title</Label>
+              <Input name="title" />
+              {getIn(touched, "title") && getIn(errors, "title") && (
+                <div className="text-red-500 text-xs">
+                  {getIn(errors, "title")}
                 </div>
-              </SortableContext>
-            </DndContext>
-          )}
-        </FieldArray>
-        <Button type="submit">Submit</Button>
-      </Form>
+              )}
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="description">Description</Label>
+              <Textarea name="description" />
+              {getIn(touched, "description") &&
+                getIn(errors, "description") && (
+                  <div className="text-red-500 text-xs">
+                    {getIn(errors, "description")}
+                  </div>
+                )}
+            </div>
+            <FormikSelect
+              label="Level"
+              name="level"
+              options={[
+                { value: "BEGINNER", label: "Beginner" },
+                { value: "INTERMEDIATE", label: "Intermediate" },
+                { value: "ADVANCED", label: "Advanced" },
+              ]}
+            />
+            {/* Weeks DnD */}
+            <h2 className="text-2xl font-semibold">Weeks</h2>
+            <FieldArray name="weeks">
+              {({ push, remove, move }) => (
+                <DndContext
+                  collisionDetection={closestCenter}
+                  onDragEnd={(event) => {
+                    const { active, over } = event;
+
+                    if (active.id !== over?.id) {
+                      const oldIndex = values.weeks.findIndex(
+                        (w) => w.id === active.id
+                      );
+                      const newIndex = values.weeks.findIndex(
+                        (w) => w.id === over?.id
+                      );
+                      move(oldIndex, newIndex);
+                      // update order
+                      console.log(
+                        arrayMove(values.weeks, oldIndex, newIndex).map(
+                          (w, i) => ({
+                            ...w,
+                            order: i,
+                          })
+                        )
+                      );
+                      setFieldValue(
+                        "weeks",
+                        arrayMove(values.weeks, oldIndex, newIndex).map(
+                          (w, i) => ({
+                            ...w,
+                            order: i,
+                          })
+                        )
+                      );
+                    }
+                  }}
+                >
+                  <SortableContext
+                    items={values.weeks.map((w) => w.id!)}
+                    strategy={verticalListSortingStrategy}
+                  >
+                    <div className="space-y-4">
+                      {values.weeks.map((week, weekIdx) => (
+                        <SortableItem
+                          key={week.order}
+                          id={week.id!}
+                          className="border border-gray-200 rounded-md px-2 py-4"
+                        >
+                          <DayFieldsForm weekIdx={weekIdx} remove={remove} />
+                        </SortableItem>
+                      ))}
+                      <Button
+                        type="button"
+                        onClick={() =>
+                          push({
+                            order: values.weeks.length,
+                            days: [],
+                            id: uuidv7(),
+                          })
+                        }
+                        className="block ml-auto"
+                      >
+                        Add Week
+                      </Button>
+                    </div>
+                  </SortableContext>
+                </DndContext>
+              )}
+            </FieldArray>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </CardContent>
+      </Card>
     </FormikProvider>
   );
 }
