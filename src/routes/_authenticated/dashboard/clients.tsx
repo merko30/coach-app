03f46301fdfb04353e8834c09c286bdb50f239b2
Merim <merim.hasanbegovic@outlook.com>
2025-09-04 13:26:28 +0200

    fix: revert

--- src/routes/_authenticated/dashboard/clients.tsx
+++ src/routes/_authenticated/dashboard/clients.tsx
@@ -1,40 +1,24 @@
-import { lazy, useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 
-import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/DataTable";
 import { clientColumns } from "@/lib/clients";
 
-import InviteClientModal from "@/components/InviteClientModal";
-
-const ClientsPage = () => {
-  const [showInviteClientModal, setShowInviteClientModal] = useState(false);
-
-  return (
-    <div className="w-full">
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          marginBottom: 16,
-        }}
-      >
-        <h2 className="text-2xl font-semibold">Clients</h2>
-        <Button onClick={() => setShowInviteClientModal(true)}>
-          Invite client
-        </Button>
-      </div>
-      <DataTable columns={clientColumns} data={[]} />
-      {showInviteClientModal && (
-        <InviteClientModal
-          open={showInviteClientModal}
-          onToggle={() => setShowInviteClientModal(false)}
-        />
-      )}
+const ClientsPage = () => (
+  <div className="w-full">
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        marginBottom: 16,
+      }}
+    >
+      <h2 className="text-2xl font-semibold">Clients</h2>
     </div>
-  );
-};
+    <DataTable columns={clientColumns} data={[]} />
+  </div>
+);
+
 export const Route = createFileRoute("/_authenticated/dashboard/clients")({
   component: ClientsPage,
 });
