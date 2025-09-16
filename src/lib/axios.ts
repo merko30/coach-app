b4603612fd5be2a0cc91626eb0c0f7f4bd080263
Merim <merim.hasanbegovic@outlook.com>
2025-09-16 15:49:42 +0200

    fix: interceptor refresh logic

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -8,4 +8,69 @@ const axiosInstance = axios.create({
   },
 });
 
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token);
+    }
+  });
+  failedQueue = [];
+};
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response &&
+      error.response.status === 401 &&
+      !originalRequest._retry
+    ) {
+      if (isRefreshing) {
+        return new Promise(function (resolve, reject) {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            if (token)
+              originalRequest.headers["Authorization"] = `Bearer ${token}`;
+            return axiosInstance(originalRequest);
+          })
+          .catch((err) => Promise.reject(err));
+      }
+
+      originalRequest._retry = true;
+      isRefreshing = true;
+      try {
+        // No need to send refresh_token, cookies are sent automatically
+        await axios.post(
+          `${import.meta.env.VITE_API_URL}/auth/refresh`,
+          {},
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+            withCredentials: true,
+          }
+        );
+        // If your backend sets new cookies, nothing else is needed
+        processQueue(null);
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        processQueue(err, null);
+        axios.post(`${import.meta.env.VITE_API_URL}/auth/logout`, {});
+        window.location.href = "/login";
+        return Promise.reject(err);
+      } finally {
+        isRefreshing = false;
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
